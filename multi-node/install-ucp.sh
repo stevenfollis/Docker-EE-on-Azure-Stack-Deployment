6e74e86d6ff96f98aadc8d35bad4fc2df5765576
Steven Follis <steven_follis@live.com>
2018-03-19 16:31:23 -0400

    Install UCP + DTR at provision time

--- multi-node/install-ucp.sh
+++ multi-node/install-ucp.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Install Docker Universal Control Plane on Ubuntu
 
 # UCP URL
-readonly UCP_URL=$1
+readonly UCP_FQDN=$1
 
 # External Service Load Balancer URL
-readonly APPS_LB_URL=$2
+readonly APPS_LB_FQDN=$2
 
 # Is node a worker or manager?
 readonly NODE_ROLE=$3
+
+# Name of current node
+readonly NODE_NAME=$(cat /etc/hostname)
 
 # Version of UCP to be installed
 readonly UCP_VERSION="3.0.0-beta3"
@@ -24,7 +27,7 @@ sudo apt-get -qq install jq -y
 checkUCP() {
 
     # Check if UCP exists by attempting to hit its load balancer
-    STATUS=$(curl --request GET --url "https://${UCP_URL}" --insecure --silent --output /dev/null -w '%{http_code}' --max-time 5)
+    STATUS=$(curl --request GET --url "https://${UCP_FQDN}" --insecure --silent --output /dev/null -w '%{http_code}' --max-time 5)
     
     if [ "$STATUS" -eq 200 ]; then
         echo "checkUCP: Successfully queried the UCP API. UCP is installed. Joining node to existing cluster."
@@ -48,8 +51,17 @@ installUCP() {
         docker/ucp:"${UCP_VERSION}" install \
         --admin-username "${UCP_ADMIN}" \
         --admin-password "${UCP_PASSWORD}" \
-        --san "${UCP_URL}" \
-        --external-service-lb "${APPS_LB_URL}"
+        --san "${UCP_FQDN}" \
+        --external-service-lb "${APPS_LB_FQDN}"
+
+    # Wait for node to reach a ready state
+    until [ $(curl --request GET --url "https://${UCP_FQDN}/_ping" --insecure --silent --header 'Accept: application/json' | grep OK) ]
+    do
+        echo '...created cluster, waiting for a ready state'
+        sleep 5
+    done
+
+    echo "installUCP: Cluster's ping returned a ready state"
 
     echo "installUCP: Finished installing Docker Universal Control Plane (UCP)"
 
@@ -58,13 +70,13 @@ installUCP() {
 joinUCP() {
 
     # Get Authentication Token
-    AUTH_TOKEN=$(curl --request POST --url "https://${UCP_URL}/auth/login" --insecure --silent --header 'Accept: application/json' --data '{ "username": "'${UCP_ADMIN}'", "password": "'${UCP_PASSWORD}'" }' | jq --raw-output .auth_token)
+    AUTH_TOKEN=$(curl --request POST --url "https://${UCP_FQDN}/auth/login" --insecure --silent --header 'Accept: application/json' --data '{ "username": "'${UCP_ADMIN}'", "password": "'${UCP_PASSWORD}'" }' | jq --raw-output .auth_token)
 
     # Get Swarm Manager IP Address + Port
-    UCP_MANAGER_ADDRESS=$(curl --request GET --url "https://${UCP_URL}/info" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq --raw-output .Swarm.RemoteManagers[0].Addr)
+    UCP_MANAGER_ADDRESS=$(curl --request GET --url "https://${UCP_FQDN}/info" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq --raw-output .Swarm.RemoteManagers[0].Addr)
     
     # Get Swarm Join Tokens
-    UCP_JOIN_TOKENS=$(curl --request GET --url "https://${UCP_URL}/swarm" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq .JoinTokens)
+    UCP_JOIN_TOKENS=$(curl --request GET --url "https://${UCP_FQDN}/swarm" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq .JoinTokens)
     UCP_JOIN_TOKEN_MANAGER=$(echo "${UCP_JOIN_TOKENS}" | jq --raw-output .Manager)
     UCP_JOIN_TOKEN_WORKER=$(echo "${UCP_JOIN_TOKENS}" | jq --raw-output .Worker)
 
@@ -77,6 +89,13 @@ joinUCP() {
         echo "joinUCP: Joining Swarm as a Worker"
         docker swarm join --token "${UCP_JOIN_TOKEN_WORKER}" "${UCP_MANAGER_ADDRESS}"
     fi
+
+    # Wait for node to reach a ready state
+    while [ "$(curl --request GET --url "https://${UCP_FQDN}/nodes/${NODE_NAME}" --insecure --silent --header 'Accept: application/json' --header "Authorization: Bearer ${AUTH_TOKEN}" | jq --raw-output .Status.State)" != "ready" ]
+    do
+        echo '...node joined, waiting for a ready state'
+        sleep 5
+    done
 
     echo "joinUCP: Finished joining node to UCP"
 
