d015d97334a5e5eba95257aaf279773441ff86ee
Steven Follis <steven.follis@docker.com>
2018-04-26 16:51:27 +0800

    Setup storage

--- multi-node/install-dtr.sh
+++ multi-node/install-dtr.sh
@@ -11,12 +11,19 @@ readonly DTR_FQDN=$2
 # Version of DTR to be installed
 readonly DTR_VERSION=$3
 
+# Azure Storage Account Credentials
+readonly AZURE_STORAGE_ACCOUNT=$4
+readonly AZURE_STORAGE_KEY=$5
+
 # Node to install DTR on
 readonly UCP_NODE=$(cat /etc/hostname)
 
 # UCP Admin credentials
 readonly UCP_USERNAME="admin"
 readonly UCP_PASSWORD='Docker123!'
+
+# Set a Replica ID 
+readonly REPLICA_ID='012345678910'
 
 checkDTR() {
 
@@ -25,9 +32,6 @@ checkDTR() {
     
     echo "checkDTR: API status for ${DTR_FQDN} returned as: ${STATUS}"
     
-    # Pre-Pull Images
-    docker run --rm docker/dtr:"${DTR_VERSION}" images --list | xargs -L 1 docker pull
-
     if [ "$STATUS" -eq 200 ]; then
         echo "checkDTR: Successfully queried the DTR API. DTR is installed. Joining node to existing cluster."
         joinDTR
@@ -47,11 +51,15 @@ installDTR() {
         --rm \
         docker/dtr:${DTR_VERSION} install \
         --dtr-external-url "https://${DTR_FQDN}" \
+        --replica-id "${REPLICA_ID}" \
         --ucp-url "https://${UCP_FQDN}" \
         --ucp-node "${UCP_NODE}" \
         --ucp-username "${UCP_USERNAME}" \
         --ucp-password "${UCP_PASSWORD}" \
         --ucp-insecure-tls 
+
+    # Configure Azure Storage
+    configureStorage
 
     echo "installDTR: Finished installing Docker Trusted Registry (DTR)"
 
@@ -60,19 +68,50 @@ installDTR() {
 joinDTR() {
 
     # Get DTR Replica ID
-    REPLICA_ID=$(curl --request GET --insecure --silent --url "https://${DTR_FQDN}/api/v0/meta/settings" -u "${UCP_USERNAME}":"${UCP_PASSWORD}" --header 'Accept: application/json' | jq --raw-output .replicaID)
     echo "joinDTR: Joining DTR with Replica ID ${REPLICA_ID}"
 
     # Join an existing Docker Trusted Registry
     docker run \
         --rm \
-        docker/dtr:${DTR_VERSION} join \
+        docker/dtr:"${DTR_VERSION}" join \
         --existing-replica-id "${REPLICA_ID}" \
         --ucp-url "https://${UCP_FQDN}" \
         --ucp-node "${UCP_NODE}" \
         --ucp-username "${UCP_USERNAME}" \
         --ucp-password "${UCP_PASSWORD}" \
         --ucp-insecure-tls
+
+}
+
+configureStorage() {
+
+    echo "Configuring DTR Storage with Azure"
+
+    # Configure DTR to use Azure Storage for its backend
+    curl --request PUT \
+    --url "https://${DTR_FQDN}/api/v0/admin/settings/registry/simple" \
+    -u "${UCP_USERNAME}:${UCP_PASSWORD}" \
+    --header 'accept: application/json' \
+    --header 'content-type: application/json' \
+    --insecure \
+    --data "{
+        \"storage\": {
+            \"azure\": {
+                \"accountkey\": \"${AZURE_STORAGE_KEY}\",
+                \"accountname\": \"${AZURE_STORAGE_ACCOUNT}\",
+                \"container\": \"dtrstorage\",
+                \"realm\": \"core.windows.net\"
+            },
+            \"delete\": {
+                \"enabled\": true
+            },
+            \"maintenance\": {
+                \"readonly\": {
+                    \"enabled\": false
+                }
+            }
+        }
+    }"
 
 }
 
